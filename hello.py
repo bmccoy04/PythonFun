2e26384b42929b709359970c7cd9a5cbc2c4c91a
bmccoy04 <bryanmccoy04@gmail.com>
2017-05-26 20:59:19 -0500

    some class stuff

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+class Weather(object):
+    def __init__(self, data):
+        self.data = data
+        self.name = 'Bryan'
+
 import requests
 
-print("Hello")
+w = Weather('data')
+
+print("Hello" + w.name)
 
 req = requests.get('http://apidev.accuweather.com/locations/v1/search?q=denham springs, la&apikey=hoArfRosT1215')
 
