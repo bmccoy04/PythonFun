b5468893b6ed045db7af124ce57e057c492db0a2
bmccoy04 <bryanmccoy04@gmail.com>
2017-05-27 15:23:02 -0500

    some list stuff.

---
+++ listsFun.py
@@ -0,0 +1,18 @@
+squares = [1, 4, 9, 16, 25]
+
+print(squares)
+
+print(squares[0])
+
+print(squares[-3:])
+
+print(squares + [36, 49, 64, 81, 100])
+
+print(len(squares))
+
+a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+print(x)
+
+print(x[0][1])
